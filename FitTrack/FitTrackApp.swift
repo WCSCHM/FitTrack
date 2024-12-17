53d3f2e19ff812859613f62407bef5acbbfa0560
WCSCHM <144642384+WCSCHM@users.noreply.github.com>
2024-12-18 00:50:54 +0800

    Initial Commit

---
+++ FitTrack/FitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrackApp.swift
+//  FitTrack
+//
+//  Created by CHwm on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct FitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
